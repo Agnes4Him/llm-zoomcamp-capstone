1ca0a787941f5028d80b6e0ff7970fac58831105
Agnes4Him <ojuhagnes@gmail.com>
2026-08-05 22:45:13 +0100

    format

--- infrastructures/providers.tf
+++ infrastructures/providers.tf
@@ -9,11 +9,11 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "healthsecure-terraform-state"
-    key            = "healthsecure/terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "healthsecure-terraform-lock"
-    encrypt        = true
+    bucket       = "healthsecure-terraform-state"
+    key          = "healthsecure/terraform.tfstate"
+    region       = "eu-west-2"
+    use_lockfile = "healthsecure-terraform-lock"
+    encrypt      = true
   }
 }
 
